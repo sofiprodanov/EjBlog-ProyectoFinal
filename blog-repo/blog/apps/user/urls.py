371850d193782f7a06c39e7328170b48df7f453a
sofiprodanov <sofiabprodanov@gmail.com>
2025-07-29 10:26:50 -0300

    Actualizaciones de user

--- blog-repo/blog/apps/user/urls.py
+++ blog-repo/blog/apps/user/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from apps.user import views as views
 
-app_name='user'
+app_name = 'user'
 
-#siempre revisar sintaxis si usamos todo plural o todo singular(users/profile > ruta del link)
 urlpatterns = [
-    path("users/profile", views.UserProfileView.as_view(), name='user_profile'),
-    
+    path('users/profile', views.UserProfileView.as_view(), name='user_profile')
 ]
--- blog-repo/blog/apps/user/views.py
+++ blog-repo/blog/apps/user/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
 
+
 class UserProfileView(TemplateView):
     template_name = 'user/user_profile.html'
-    
