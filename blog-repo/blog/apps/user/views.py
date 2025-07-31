6b5898720ff845e57b3687d326192c9999e66a32
sofiprodanov <sofiabprodanov@gmail.com>
2025-07-31 10:24:04 -0300

    Se agregan clases, funcionalidades y botones de Inicio de Sesion, Cierre de sesion y Registracion

--- blog-repo/blog/apps/user/views.py
+++ blog-repo/blog/apps/user/views.py
@@ -1,5 +1,34 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from apps.user.forms import RegisterForm, LoginForm
+from django.urls import reverse_lazy
+from django.contrib.auth.models import Group
+from django.contrib.auth.views import LoginView as LoginViewDjango,LogoutViewDjango
 
 
 class UserProfileView(TemplateView):
     template_name = 'user/user_profile.html'
+
+class RegisterView(CreateView):
+    template_name = 'auth/auth_register.html'
+    form_class = RegisterForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        
+        registered_group = Group.objects.get(name="Registered")
+
+        self.objetc.groups.add(registered_group)
+
+        return response
+
+class LoginView(LoginViewDjango):
+    template_name = 'auth/auth_login.html'
+    authentication_form = LoginForm
+    
+    def get_success_url(self):
+        return reverse_lazy('home')
+    
+class LogoutView(LogoutViewDjango):
+    def get_next_page(self):
+        return reverse_lazy('home')
