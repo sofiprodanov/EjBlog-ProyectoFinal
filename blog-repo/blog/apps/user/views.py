bd7f3d1a514737fd7fc5b48dc5f26695c8d9b579
sofiprodanov <sofiabprodanov@gmail.com>
2025-07-31 10:41:00 -0300

    Se realizan correccion de errores sobre la redireccion

--- blog-repo/blog/apps/user/views.py
+++ blog-repo/blog/apps/user/views.py
@@ -2,8 +2,9 @@ from django.views.generic import TemplateView, CreateView
 from apps.user.forms import RegisterForm, LoginForm
 from django.urls import reverse_lazy
 from django.contrib.auth.models import Group
-from django.contrib.auth.views import LoginView as LoginViewDjango,LogoutViewDjango
-
+from django.contrib.auth.views import LoginView as LoginViewDjango,LogoutView
+from django.contrib.auth import logout
+from django.shortcuts import redirect
 
 class UserProfileView(TemplateView):
     template_name = 'user/user_profile.html'
@@ -23,12 +24,16 @@ class RegisterView(CreateView):
         return response
 
 class LoginView(LoginViewDjango):
+    #verificar el rol del usuario
     template_name = 'auth/auth_login.html'
     authentication_form = LoginForm
     
     def get_success_url(self):
         return reverse_lazy('home')
     
-class LogoutView(LogoutViewDjango):
-    def get_next_page(self):
-        return reverse_lazy('home')
+# class LogoutView(LogoutViewDjango):
+#     def get_next_page(self):
+
+def LogoutView(request):
+    logout(request)
+    return redirect('home')
