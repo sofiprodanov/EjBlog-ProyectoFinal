7f71c80d7191befa67efd4f8f788c5b06c677e88
sofiprodanov <sofiabprodanov@gmail.com>
2025-08-05 10:16:13 -0300

    Clase 05/08

--- blog-repo/blog/apps/post/urls.py
+++ blog-repo/blog/apps/post/urls.py
@@ -5,6 +5,9 @@ app_name='post'
 
 #siempre revisar sintaxis si usamos todo plural o todo singular(users/profile > ruta del link)
 urlpatterns = [
+    path("posts/", views.PostListView.as_view(), name='post_list'),
     path("posts/<slug:slug>", views.PostDetailView.as_view(), name='post_detail'),
+    path("posts/<slug:slug>/delete", views.PostDeleteView.as_view(), name='post_delete'),
+    path("posts/<slug:slug>/update", views.PostUpdateView.as_view(), name='post_update'),
     
 ]
